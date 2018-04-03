a5e959bc8b38b4dcd8b9150e203e63dda4096889
anharaf <35832706+anharaf@users.noreply.github.com>
2018-04-03 21:40:22 +0700

    NodeMCU get yaw,pitch,fire
    mendapatkan data dari hololens berupa nilai yaw, pitch, fire dalam bentuk json kemudian di parsing

---
+++ Arduino/Demo/sketch_3April.ino
@@ -0,0 +1,120 @@
+#include <WiFiUdp.h>
+#include <WiFiServer.h>
+#include <WiFiClientSecure.h>
+#include <WiFiClient.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+
+// Update these with values suitable for your network.
+
+const char* ssid = "Titanic";
+const char* password = "samakayakhotspotitb";
+const char* mqtt_server = "192.168.0.109";
+const char* mqtt_userName = "anharaf";
+const char* mqtt_password = "anhar1234";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  //Serial.print("Message arrived [");
+  //Serial.print(topic);
+  //Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    //Serial.print((char)payload[i]);
+  }
+  Serial.println();
+  //payload[length] = '\0';
+  //Serial.print("payload : [");
+  //Serial.print((char *)payload);
+  //Serial.print("]");
+  StaticJsonBuffer<300> JSONBuffer;   //Memory pool
+  JsonObject& parsed = JSONBuffer.parseObject((char *)payload); //Parse message
+  
+  if (!parsed.success()) {
+    Serial.println("parseObject() failed");
+    return;
+  }
+  
+  int yaw = parsed["yaw"];
+  int pitch= parsed["pitch"];
+  int fire=parsed["fire"];
+
+  Serial.println(yaw);
+  Serial.println(pitch);
+  Serial.println(fire);
+}
+
+void setup() {
+  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  
+  client.setCallback(callback);
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client", mqtt_userName, mqtt_password)) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      client.subscribe("inTopic");
+    }
+    else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  if (now - lastMsg > 2000) {
+    lastMsg = now;
+    ++value;
+    snprintf(msg, 75, "hello world #%ld", value);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish("outTopic", msg);
+  }
+}
